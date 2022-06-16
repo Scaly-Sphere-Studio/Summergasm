c07ef257312b20cad814fab46ad0aee26b5acd93
Mériadec Lantonnet <meriadeclantonnet@gmail.com>
2022-06-16 16:39:05 +0200

    Finish audio Source UI (handle buffers)

--- src/imgui/audio.cpp
+++ src/imgui/audio.cpp
@@ -38,6 +38,110 @@ static void print_object(SSS::Audio::Source::Ptr const& source)
     if (ImGui::Checkbox(" Loop", &loop)) {
         source->setLooping(loop);
     }
+
+    SSS::Audio::Buffer::Map const& buffers = SSS::Audio::getBuffers();
+    std::vector<uint32_t> buffer_ids = source->getBufferIDs();
+
+    if (buffers.empty()) {
+            ImGui::Text("No audio buffer exists.");
+    }
+    else if (ImGui::SmallButton("Add buffer")) {
+        uint32_t new_id = buffers.cbegin()->first;
+        for (auto const& pair : buffers) {
+            bool found_similar = false;
+            for (uint32_t id : buffer_ids) {
+                if (pair.first == id) {
+                    found_similar = true;
+                    break;
+                }
+            }
+            if (!found_similar) {
+                new_id = pair.first;
+                break;
+            }
+        }
+        source->queueBuffers(std::vector<uint32_t>(1, new_id));
+    }
+
+    // Display each chunk object in an organizable single column table
+    if (!buffer_ids.empty() && ImGui::BeginTable("##", 3, table_flags)) {
+        char label[256];
+        static bool hold_state = false;
+        static size_t hold_i = 0;
+        if (hold_state && !ImGui::IsMouseDown(0)) {
+            hold_state = false;
+        }
+        if (hold_state) {
+            SetCursor(ui_window, GLFW_HAND_CURSOR);
+        }
+
+        char drag_drop_id[256];
+        sprintf_s(drag_drop_id, "Buffer_ID_Dragging");
+        for (size_t i = 0; i < buffer_ids.size(); ) {
+            ImGui::TableNextRow();
+            ImGui::TableSetColumnIndex(0);
+            // Current object ID
+            uint32_t& id = buffer_ids.at(i);
+            // Selectable showing ID
+            sprintf_s(label, "Buffer %u", id);
+            bool selected = hold_state && hold_i == i;
+            ImGui::Selectable(label, selected);
+            if (ImGui::IsItemHovered()) {
+                SetCursor(ui_window, GLFW_HAND_CURSOR);
+            }
+            Tooltip("Drag to reorder");
+            if (ImGui::BeginDragDropSource(ImGuiDragDropFlags_SourceNoPreviewTooltip
+                | ImGuiDragDropFlags_SourceNoDisableHover
+                | ImGuiDragDropFlags_SourceNoHoldToOpenOthers))
+            {
+                // Give dragged source value
+                ImGui::SetDragDropPayload(drag_drop_id, nullptr, 0);
+                hold_state = true;
+                hold_i = i;
+                ImGui::EndDragDropSource();
+            }
+            if (ImGui::BeginDragDropTarget()) {
+                if (hold_i < buffer_ids.size()) {
+                    int incr = hold_i < i ? 1 : -1;
+                    for (size_t k = hold_i; k != i; k += incr) {
+                        std::swap(buffer_ids.at(k), buffer_ids.at(k + incr));
+                        source->detachBuffers();
+                        source->queueBuffers(buffer_ids);
+                    }
+                }
+                ImGui::EndDragDropTarget();
+            }
+            // Edit element
+            ImGui::TableSetColumnIndex(1);
+            char popup_map_id[256];
+            sprintf_s(popup_map_id, "##edit_buffer_id%zu", i);
+            sprintf_s(label, "*##edit_buffer_id%zu", i);
+            if (Tooltip("Edit", ImGui::SmallButton, label)) {
+                ImGui::OpenPopup(popup_map_id);
+            }
+            if (ImGui::BeginPopup(popup_map_id)) {
+                ImGui::SetNextItemWidth(100.f);
+                if (MapIDCombo(popup_map_id, SSS::Audio::getBuffers(), id)) {
+                    source->detachBuffers();
+                    source->queueBuffers(buffer_ids);
+                    ImGui::CloseCurrentPopup();
+                }
+                ImGui::EndPopup();
+            }
+            // Delete element
+            ImGui::TableSetColumnIndex(2);
+            sprintf_s(label, "×##delete_buffer_id%zu", i);
+            if (Tooltip("Delete", ImGui::SmallButton, label)) {
+                buffer_ids.erase(buffer_ids.begin() + i);
+                source->detachBuffers();
+                source->queueBuffers(buffer_ids);
+            }
+            else
+                ++i;
+        }
+        ImGui::EndTable();
+    }
+
 }
 
 template<>
