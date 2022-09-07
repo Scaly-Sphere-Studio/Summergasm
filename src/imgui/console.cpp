586b2a1e8aa0331e816455e9c8e9b1f3c475fd6d
Mériadec Lantonnet <meriadeclantonnet@gmail.com>
2022-09-07 19:45:37 +0200

    Add history to console

--- src/imgui/console.cpp
+++ src/imgui/console.cpp
@@ -14,13 +14,13 @@ void print_console() {
         return;
     }
 
-    // ImGui g_data->window presets (cropped to GL window)
+    // Crop to bottom of Window
     int ui_w, ui_h;
     g_data->window->getDimensions(ui_w, ui_h);
     ImGui::SetNextWindowSize(ImVec2(static_cast<float>(ui_w), static_cast<float>(300)));
     ImGui::SetNextWindowPos(ImVec2(0, ui_h - 300));
     constexpr ImGuiWindowFlags flags = 0
-        | ImGuiWindowFlags_NoTitleBar
+        | ImGuiWindowFlags_NoCollapse
         | ImGuiWindowFlags_NoResize
         | ImGuiWindowFlags_NoMove
     ;
@@ -28,6 +28,7 @@ void print_console() {
 
     // Render UI
     if (ImGui::Begin("Console", nullptr, flags)) {
+
         struct CmdMemory {
             CmdMemory(std::string const& s) : str(s) {};
             std::string str;
@@ -35,22 +36,74 @@ void print_console() {
         };
         static std::vector<CmdMemory> memory;
         for (CmdMemory const& cmd : memory) {
-            ImGui::TextColored(cmd.color, cmd.str.c_str());
+            std::string str("> " + cmd.str);
+            ImGui::TextColored(cmd.color, str.c_str());
         }
         static char buffer[4096];
+        static size_t memory_index = 0;
+        static std::string buffer_memory;
+        
         ImGui::SetNextItemWidth(-1);
         ImGui::SetCursorPos(ImVec2(0, 272));
         ImGui::SetKeyboardFocusHere();
-        ImGui::InputText("##console_text", buffer, 4096);
-        if (ImGui::IsItemDeactivatedAfterEdit()) {
-            memory.emplace_back(buffer);
-            try {
-                g_data->lua.script(buffer);
+        constexpr auto flags = 0
+            | ImGuiInputTextFlags_CallbackHistory
+            | ImGuiInputTextFlags_CallbackCompletion
+            | ImGuiInputTextFlags_CallbackEdit
+        ;
+        // Text input, with internal callback
+        ImGui::InputText("##console_text", buffer, 4096, flags,
+            [](ImGuiInputTextCallbackData* data)->int
+            {
+                switch (data->EventFlag) {
+                // History
+                case ImGuiInputTextFlags_CallbackHistory: {
+                    if (memory.empty()) break;
+                    // Up
+                    if (data->EventKey == ImGuiKey_UpArrow && memory_index < memory.size())
+                        ++memory_index;
+                    // Down
+                    if (data->EventKey == ImGuiKey_DownArrow) {
+                        if (memory_index != 0) --memory_index;
+                        if (memory_index == 0) {
+                            data->DeleteChars(0, data->BufTextLen);
+                            data->InsertChars(0, buffer_memory.c_str());
+                            break;
+                        }
+                    }
+                    // ... Print
+                    data->DeleteChars(0, data->BufTextLen);
+                    if (memory_index != 0) {
+                        std::string const& str = memory.at(memory.size() - memory_index).str;
+                        data->InsertChars(0, str.c_str());
+                    }
+                }   break;
+                // Completion
+                case ImGuiInputTextFlags_CallbackCompletion: {
+                    // TODO
+                }   break;
+                // Normal edit
+                case ImGuiInputTextFlags_CallbackEdit: {
+                    memory_index = 0;
+                    buffer_memory = data->Buf;
+                }   break;
+                }
+                return 0;
             }
-            catch (...) {
+        );
+        // Enter after edit
+        if (ImGui::IsItemDeactivatedAfterEdit()) {
+            // Add command to memory
+            memory.emplace_back(buffer);
+            auto result = g_data->lua.safe_script(buffer, sol::script_pass_on_error);
+            if (!result.valid()) {
                 memory.back().color = ImVec4(1, 0, 0, 1);
+                sol::error err = result;
+                LOG_CTX_ERR("Lua console", err.what());
             }
             buffer[0] = 0;
+            memory_index = 0;
+            buffer_memory.clear();
         }
         ImGui::End();
     }
