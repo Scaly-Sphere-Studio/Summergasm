6f298adaa23e20afb874a11e879e9d1402836d1c
Mériadec Lantonnet <meriadeclantonnet@gmail.com>
2023-06-04 15:53:59 +0200

    Refactor Console into OOP

--- src/imgui/console.cpp
+++ src/imgui/console.cpp
@@ -8,62 +8,77 @@ struct CmdMemory {
     ImVec4 color{ 1, 1, 1, 1 };
 };
 
-static std::vector<CmdMemory> memory;
+struct ConsoleMemory {
+    std::vector<CmdMemory> cmds;
+    size_t index{ 0 };
 
-static char buffer[4096];
-static size_t memory_index = 0;
-static std::string buffer_memory;
+    void callback(ImGuiInputTextCallbackData* data, std::string const& buffer);
+    void pushCmd(std::string const& buffer);
+};
 
-static std::unique_ptr<LuaConsoleData const> console_data;
-static size_t cursor = 0;
-static bool reset_cursor = true;
-static std::string last_key;
+struct ConsoleAutocomplete {
+    std::unique_ptr<LuaConsoleData const> all_keys;
+    size_t cursor{ 0 };
+    bool reset_cursor{ true };
+    std::string last_key;
 
-static int inputTextCallback(ImGuiInputTextCallbackData* data)
+    void callback(ImGuiInputTextCallbackData* data, std::string const& buffer);
+};
+
+struct Console {
+    ConsoleMemory memory;
+    ConsoleAutocomplete complete;
+    std::string buffer;
+    int callback(ImGuiInputTextCallbackData* data);
+    static int static_callback(ImGuiInputTextCallbackData* data);
+};
+
+
+void ConsoleMemory::callback(ImGuiInputTextCallbackData* data, std::string const& buffer)
 {
-    if (data->EventFlag != ImGuiInputTextFlags_CallbackCompletion || reset_cursor) {
-        if (console_data) {
-            if (!last_key.empty()) {
-                buffer_memory = data->Buf;
-                last_key.clear();
-            }
-            console_data.reset();
+    if (cmds.empty()) return;
+    // Up
+    if (data->EventKey == ImGuiKey_UpArrow && index < cmds.size())
+        ++index;
+    // Down
+    if (data->EventKey == ImGuiKey_DownArrow) {
+        if (index != 0)
+            --index;
+        if (index == 0) {
+            data->DeleteChars(0, data->BufTextLen);
+            data->InsertChars(0, buffer.c_str());
+            return;
         }
-        cursor = data->CursorPos;
-        reset_cursor = false;
     }
+    // ... Print
+    data->DeleteChars(0, data->BufTextLen);
+    if (index != 0) {
+        std::string const& str = cmds.at(cmds.size() - index).str;
+        data->InsertChars(0, str.c_str());
+    }
+}
 
-    switch (data->EventFlag) {
+void ConsoleMemory::pushCmd(std::string const& buffer)
+{
+    cmds.emplace_back(buffer);
+    auto result = g->lua.safe_script(buffer, *mylua_console_env, sol::script_pass_on_error);
+    if (!result.valid()) {
+        cmds.back().color = ImVec4(1, 0, 0, 1);
+        sol::error err = result;
+        LOG_CTX_ERR("Lua console", std::string("\n") + err.what());
+    }
+    index = 0;
+}
 
-    // History
-    case ImGuiInputTextFlags_CallbackHistory: {
-        if (memory.empty()) break;
-        // Up
-        if (data->EventKey == ImGuiKey_UpArrow && memory_index < memory.size())
-            ++memory_index;
-        // Down
-        if (data->EventKey == ImGuiKey_DownArrow) {
-            if (memory_index != 0) --memory_index;
-            if (memory_index == 0) {
-                data->DeleteChars(0, data->BufTextLen);
-                data->InsertChars(0, buffer_memory.c_str());
-                break;
-            }
-        }
-        // ... Print
-        data->DeleteChars(0, data->BufTextLen);
-        if (memory_index != 0) {
-            std::string const& str = memory.at(memory.size() - memory_index).str;
-            data->InsertChars(0, str.c_str());
-        }
-    }   break;
 
-    // Completion
-    case ImGuiInputTextFlags_CallbackCompletion: {
+void ConsoleAutocomplete::callback(ImGuiInputTextCallbackData* data, std::string const& buffer)
+{
+    std::string const buf = buffer.substr(0, cursor);
 
-        std::string const buf = buffer_memory.substr(0, cursor);
-        
-        std::string const current = [](std::string buf) {
+    auto const [table, key, separator] = [buf]() {
+        std::regex const r("[.:]");
+
+        std::string key = [](std::string buf) {
             std::regex const r("[ (]");
             std::smatch sm;
             while (std::regex_search(buf, sm, r)) {
@@ -72,75 +87,95 @@ static int inputTextCallback(ImGuiInputTextCallbackData* data)
             return buf;
         }(buf);
 
-        auto const [table_name, key, separator] = [current]() {
-            std::regex const r("[.:]");
-            std::string key = current;
-            std::string table;
-            char separator = '.';
-            std::smatch sm;
-            while (std::regex_search(key, sm, r)) {
-                if (!table.empty()) {
-                    table += separator;
-                }
-                separator = sm[0].str().at(0);
-                table += sm.prefix().str();
-                key = sm.suffix().str();
+        std::string table;
+        char separator = '.';
+        std::smatch sm;
+        while (std::regex_search(key, sm, r)) {
+            if (!table.empty()) {
+                table += separator;
             }
-            return std::make_tuple(table, key, separator);
-        }();
-
-        if (!console_data) {
-            sol::environment const env = *mylua_console_env;
-            sol::table t = env;
-            if (!table_name.empty()) {
-                auto res = g->lua.safe_script("return " + table_name, env, sol::script_pass_on_error);
-                if (!res.valid() || (res.get_type() != sol::type::table &&
-                    res.get_type() != sol::type::userdata))
-                {
-                    break;
-                }
-                t = res;
-            }
-            console_data = std::make_unique<LuaConsoleData const>(g->lua, t, env, table_name);
+            separator = sm[0].str().at(0);
+            table += sm.prefix().str();
+            key = sm.suffix().str();
         }
+        return std::make_tuple(table, key, separator);
+    }();
 
-        if (!last_key.empty()) {
-            data->DeleteChars(buf.size() , last_key.size() - key.size());
-            auto it = console_data->find(last_key);
-            if (it != console_data->cend()) {
+    if (!all_keys) {
+        sol::environment const env = *mylua_console_env;
+        sol::table t = env;
+        if (!table.empty()) {
+            auto res = g->lua.safe_script("return " + table, env, sol::script_pass_on_error);
+            if (!res.valid() || (res.get_type() != sol::type::table &&
+                res.get_type() != sol::type::userdata))
+                return;
+            t = res;
+        }
+        all_keys = std::make_unique<LuaConsoleData const>(g->lua, t, env, table);
+    }
+
+    if (!last_key.empty()) {
+        data->DeleteChars(buf.size(), last_key.size() - key.size());
+        auto it = all_keys->find(last_key);
+        if (it != all_keys->cend()) {
+            ++it;
+            while (it != all_keys->cend() && it->second.separator != separator)
                 ++it;
-                while (it != console_data->cend() && it->second.separator != separator)
-                    ++it;
-                if (it != console_data->cend()) {
-                    std::string const& new_key = it->first;
-                    if (new_key.find(key) == 0) {
-                        data->InsertChars(buf.size(), new_key.c_str() + key.size());
-                        last_key = new_key;
-                        break;
-                    }
+            if (it != all_keys->cend()) {
+                std::string const& new_key = it->first;
+                if (new_key.find(key) == 0) {
+                    data->InsertChars(buf.size(), new_key.c_str() + key.size());
+                    last_key = new_key;
+                    return;
                 }
             }
         }
+    }
 
-        for (auto const& [k, v] : *console_data) {
-            if (v.separator == separator && k.find(key) == 0 &&
-                (!last_key.empty() || k != current))
-            {
-                data->InsertChars(buf.size(), k.c_str() + key.size());
-                last_key = k;
-                break;
-            }
+    for (auto const& [k, v] : *all_keys) {
+        if (v.separator == separator && k.find(key) == 0 && (!last_key.empty() || k != key)) {
+            data->InsertChars(buf.size(), k.c_str() + key.size());
+            last_key = k;
+            return;
         }
+    }
+}
 
-    }   break;
-    
+int Console::callback(ImGuiInputTextCallbackData* data)
+{
+    if (data->EventFlag != ImGuiInputTextFlags_CallbackCompletion || complete.reset_cursor) {
+        if (complete.all_keys) {
+            if (!complete.last_key.empty()) {
+                buffer = data->Buf;
+                complete.last_key.clear();
+            }
+            complete.all_keys.reset();
+        }
+        complete.cursor = data->CursorPos;
+        complete.reset_cursor = false;
+    }
+
+    switch (data->EventFlag) {
+    // History
+    case ImGuiInputTextFlags_CallbackHistory:
+        memory.callback(data, buffer);
+        break;
+    // Completion
+    case ImGuiInputTextFlags_CallbackCompletion:
+        complete.callback(data, buffer);
+        break;
     // Normal edit
-    case ImGuiInputTextFlags_CallbackEdit: {
-        memory_index = 0;
-        buffer_memory = data->Buf;
-    }   break;
+    case ImGuiInputTextFlags_CallbackEdit:
+        memory.index = 0;
+        buffer = data->Buf;
+        break;
     }
     return 0;
+}
+
+int Console::static_callback(ImGuiInputTextCallbackData* data)
+{
+    return reinterpret_cast<Console*>(data->UserData)->callback(data);
 }
 
 void print_console()
@@ -150,6 +185,8 @@ void print_console()
         g->window->unblockInputs();
         return;
     }
+
+    static Console console;
 
     // Make context current
     SSS::GL::Context const context = g->window->setContext();
@@ -176,48 +213,42 @@ void print_console()
         if (ImGui::BeginChild("##memory", ImVec2(-FLT_MIN, 260), false,
             ImGuiWindowFlags_AlwaysUseWindowPadding))
         {
-            for (CmdMemory const& cmd : memory) {
+            auto const& cmds = console.memory.cmds;
+
+            for (CmdMemory const& cmd : cmds) {
                 std::string str("> " + cmd.str);
                 ImGui::TextColored(cmd.color, str.c_str());
             }
+            
             static size_t mem_size = 0;
-            if (mem_size !=  memory.size()) {
-                mem_size = memory.size();
+            if (mem_size != cmds.size()) {
+                mem_size = cmds.size();
                 ImGui::SetScrollHereY();
             }
         }
         ImGui::EndChild();
-        
+
         ImGui::SetNextItemWidth(-1);
         ImGui::SetKeyboardFocusHere();
         constexpr auto flags = 0
             | ImGuiInputTextFlags_CallbackHistory
             | ImGuiInputTextFlags_CallbackCompletion
             | ImGuiInputTextFlags_CallbackEdit
-        ;
+            ;
 
         if (glfwGetKey(g->window->getGLFWwindow(), GLFW_KEY_LEFT) ||
             glfwGetKey(g->window->getGLFWwindow(), GLFW_KEY_RIGHT)) {
-            reset_cursor = true;
+            console.complete.reset_cursor = true;
         }
 
+        static char buffer[4096];
         // Text input, with internal callback
-        ImGui::InputText("##console_text", buffer, 4096, flags, inputTextCallback);
+        ImGui::InputText("##console_text", buffer, 4096, flags, Console::static_callback, &console);
         // Enter after edit
         if (ImGui::IsItemDeactivatedAfterEdit()) {
-            // Add command to memory
-            memory.emplace_back(buffer);
-            auto result = g->lua.safe_script(buffer, *mylua_console_env, sol::script_pass_on_error);
-            if (!result.valid()) {
-                memory.back().color = ImVec4(1, 0, 0, 1);
-                sol::error err = result;
-                LOG_CTX_ERR("Lua console", std::string("\n") + err.what());
-            }
+            console.memory.pushCmd(buffer);
             buffer[0] = 0;
-            memory_index = 0;
-            buffer_memory.clear();
-            console_data.reset();
-            last_key.clear();
+            console.buffer.clear();
         }
         ImGui::End();
     }
